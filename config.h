ad925e8d93a22c469c53e7f318d9d89d2e49c838
vimnoob <riley199@protonmail.com>
2025-07-08 19:33:03 +1200

    added keybind for slock

--- config.h
+++ config.h
@@ -89,6 +89,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ 0,                            XK_Print,  spawn,          SHCMD("flameshot gui") },
+	{ MODKEY,                       XK_l,      spawn,          SHCMD("slock") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
